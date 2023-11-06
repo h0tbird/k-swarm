969bf00fa7209b134480553f463f58221ecbfc90
Marc Villacorta <h0tbird@github.com>
2023-11-06 15:35:40 +0100

    kubebuilder create api

--- cmd/main.go
+++ cmd/main.go
@@ -31,6 +31,8 @@ import (
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
+
+	"github.com/octoroot/swarm/internal/controller"
 	//+kubebuilder:scaffold:imports
 )
 
@@ -85,6 +87,13 @@ func main() {
 		os.Exit(1)
 	}
 
+	if err = (&controller.ServiceReconciler{
+		Client: mgr.GetClient(),
+		Scheme: mgr.GetScheme(),
+	}).SetupWithManager(mgr); err != nil {
+		setupLog.Error(err, "unable to create controller", "controller", "Service")
+		os.Exit(1)
+	}
 	//+kubebuilder:scaffold:builder
 
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
