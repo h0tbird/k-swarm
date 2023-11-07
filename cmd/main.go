090c14c3e9dd6a37a2c3ffbc0366f63177d10e27
Marc Villacorta <h0tbird@github.com>
2023-11-07 12:17:06 +0100

    Handle flags and logger

--- cmd/main.go
+++ cmd/main.go
@@ -17,21 +17,24 @@ limitations under the License.
 package main
 
 import (
+
+	// Stdlib
 	"flag"
 	"os"
+	"time"
 
-	// Import all Kubernetes client auth plugins (e.g. Azure, GCP, OIDC, etc.)
-	// to ensure that exec-entrypoint and run can make use of them.
-	_ "k8s.io/client-go/plugin/pkg/client/auth"
-
+	// Community
+	"github.com/spf13/pflag"
 	"k8s.io/apimachinery/pkg/runtime"
 	utilruntime "k8s.io/apimachinery/pkg/util/runtime"
 	clientgoscheme "k8s.io/client-go/kubernetes/scheme"
+	_ "k8s.io/client-go/plugin/pkg/client/auth"
 	ctrl "sigs.k8s.io/controller-runtime"
 	"sigs.k8s.io/controller-runtime/pkg/healthz"
 	"sigs.k8s.io/controller-runtime/pkg/log/zap"
 	metricsserver "sigs.k8s.io/controller-runtime/pkg/metrics/server"
 
+	// Internal
 	"github.com/octoroot/swarm/internal/controller"
 	//+kubebuilder:scaffold:imports
 )
@@ -47,46 +50,79 @@ func init() {
 	//+kubebuilder:scaffold:scheme
 }
 
+//-----------------------------------------------------------------------------
+// Global variables
+//-----------------------------------------------------------------------------
+
+var (
+	metricsAddr          string
+	enableLeaderElection bool
+	probeAddr            string
+	syncPeriod           time.Duration
+)
+
+//-----------------------------------------------------------------------------
+// initFlags initializes the command line flags.
+//-----------------------------------------------------------------------------
+
+func initFlags(fs *pflag.FlagSet) {
+	flag.StringVar(
+		&metricsAddr,
+		"metrics-bind-address",
+		":8080",
+		"The address the metric endpoint binds to.")
+
+	flag.StringVar(
+		&probeAddr,
+		"health-probe-bind-address",
+		":8081",
+		"The address the probe endpoint binds to.")
+
+	flag.BoolVar(
+		&enableLeaderElection,
+		"leader-elect",
+		false,
+		"Enable leader election for controller manager. Enabling this will ensure there is only one active controller manager.")
+
+	fs.DurationVar(
+		&syncPeriod,
+		"sync-period",
+		10*time.Hour,
+		"The minimum interval at which watched resources are reconciled.")
+}
+
+//-----------------------------------------------------------------------------
+// main
+//-----------------------------------------------------------------------------
+
 func main() {
-	var metricsAddr string
-	var enableLeaderElection bool
-	var probeAddr string
-	flag.StringVar(&metricsAddr, "metrics-bind-address", ":8080", "The address the metric endpoint binds to.")
-	flag.StringVar(&probeAddr, "health-probe-bind-address", ":8081", "The address the probe endpoint binds to.")
-	flag.BoolVar(&enableLeaderElection, "leader-elect", false,
-		"Enable leader election for controller manager. "+
-			"Enabling this will ensure there is only one active controller manager.")
-	opts := zap.Options{
-		Development: true,
-	}
-	opts.BindFlags(flag.CommandLine)
-	flag.Parse()
 
-	ctrl.SetLogger(zap.New(zap.UseFlagOptions(&opts)))
+	zapOpts := zap.Options{}
 
+	// Handle flags
+	initFlags(pflag.CommandLine)
+	zapOpts.BindFlags(flag.CommandLine)
+	pflag.CommandLine.AddGoFlagSet(flag.CommandLine)
+	pflag.Parse()
+
+	// Logger setup
+	logr := zap.New(zap.UseFlagOptions(&zapOpts))
+	ctrl.SetLogger(logr)
+
+	// Initializes a new controller manager
 	mgr, err := ctrl.NewManager(ctrl.GetConfigOrDie(), ctrl.Options{
 		Scheme:                 scheme,
 		Metrics:                metricsserver.Options{BindAddress: metricsAddr},
 		HealthProbeBindAddress: probeAddr,
 		LeaderElection:         enableLeaderElection,
 		LeaderElectionID:       "bb4dbf8a.github.com",
-		// LeaderElectionReleaseOnCancel defines if the leader should step down voluntarily
-		// when the Manager ends. This requires the binary to immediately end when the
-		// Manager is stopped, otherwise, this setting is unsafe. Setting this significantly
-		// speeds up voluntary leader transitions as the new leader don't have to wait
-		// LeaseDuration time first.
-		//
-		// In the default scaffold provided, the program ends immediately after
-		// the manager stops, so would be fine to enable this option. However,
-		// if you are doing or is intended to do any operation such as perform cleanups
-		// after the manager stops then its usage might be unsafe.
-		// LeaderElectionReleaseOnCancel: true,
 	})
 	if err != nil {
 		setupLog.Error(err, "unable to start manager")
 		os.Exit(1)
 	}
 
+	// Register the service controller
 	if err = (&controller.ServiceReconciler{
 		Client: mgr.GetClient(),
 		Scheme: mgr.GetScheme(),
@@ -96,6 +132,7 @@ func main() {
 	}
 	//+kubebuilder:scaffold:builder
 
+	// Add health and ready checks
 	if err := mgr.AddHealthzCheck("healthz", healthz.Ping); err != nil {
 		setupLog.Error(err, "unable to set up health check")
 		os.Exit(1)
@@ -105,6 +142,7 @@ func main() {
 		os.Exit(1)
 	}
 
+	// Start the manager
 	setupLog.Info("starting manager")
 	if err := mgr.Start(ctrl.SetupSignalHandler()); err != nil {
 		setupLog.Error(err, "problem running manager")
--- internal/controller/service_controller.go
+++ internal/controller/service_controller.go
@@ -17,8 +17,11 @@ limitations under the License.
 package controller
 
 import (
+
+	// Stdlib
 	"context"
 
+	// Community
 	corev1 "k8s.io/api/core/v1"
 	"k8s.io/apimachinery/pkg/runtime"
 	ctrl "sigs.k8s.io/controller-runtime"
@@ -32,29 +35,36 @@ type ServiceReconciler struct {
 	Scheme *runtime.Scheme
 }
 
+const (
+	appLabel = "swarm"
+)
+
 //+kubebuilder:rbac:groups=core,resources=services,verbs=get;list;watch;create;update;patch;delete
 //+kubebuilder:rbac:groups=core,resources=services/status,verbs=get;update;patch
 //+kubebuilder:rbac:groups=core,resources=services/finalizers,verbs=update
 
-// Reconcile is part of the main kubernetes reconciliation loop which aims to
-// move the current state of the cluster closer to the desired state.
-// TODO(user): Modify the Reconcile function to compare the state specified by
-// the Service object against the actual cluster state, and then
-// perform operations to make the cluster state reflect the state specified by
-// the user.
-//
-// For more details, check Reconcile and its Result here:
-// - https://pkg.go.dev/sigs.k8s.io/controller-runtime@v0.16.3/pkg/reconcile
+//-----------------------------------------------------------------------------
+// Reconcile is part of the main kubernetes reconciliation loop.
+//-----------------------------------------------------------------------------
+
 func (r *ServiceReconciler) Reconcile(ctx context.Context, req ctrl.Request) (ctrl.Result, error) {
 	log := log.FromContext(ctx)
 
-	// TODO(user): your logic here
-	log.Info("Reconciling Service")
+	// Get all the swarm services
+	var services corev1.ServiceList
+	if err := r.List(ctx, &services, client.MatchingLabels{"app": appLabel}); err != nil {
+		log.Error(err, "unable to list services")
+		return ctrl.Result{}, err
+	}
 
+	// Return on success
 	return ctrl.Result{}, nil
 }
 
+//-----------------------------------------------------------------------------
 // SetupWithManager sets up the controller with the Manager.
+//-----------------------------------------------------------------------------
+
 func (r *ServiceReconciler) SetupWithManager(mgr ctrl.Manager) error {
 	return ctrl.NewControllerManagedBy(mgr).
 		For(&corev1.Service{}).
