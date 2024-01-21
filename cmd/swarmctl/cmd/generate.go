93190dc27f31b1da067d69e15a84ad901d172660
Marc Villacorta <h0tbird@github.com>
2024-01-21 20:34:43 +0100

    Remove comments

--- cmd/swarmctl/cmd/generate.go
+++ cmd/swarmctl/cmd/generate.go
@@ -30,14 +30,4 @@ var generateCmd = &cobra.Command{
 
 func init() {
 	manifestCmd.AddCommand(generateCmd)
-
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// generateCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// generateCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
