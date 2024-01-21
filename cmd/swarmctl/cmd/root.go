688a32bcafa20c05baf19abaf74c9f1ddf9372fe
Marc Villacorta <h0tbird@github.com>
2024-01-22 00:54:53 +0100

    Initial contextCompletionFunc()

--- cmd/swarmctl/cmd/root.go
+++ cmd/swarmctl/cmd/root.go
@@ -5,6 +5,7 @@ import (
 	// Stdlib
 	"embed"
 	"os"
+	"strings"
 
 	// Community
 	"github.com/spf13/cobra"
@@ -38,5 +39,30 @@ func Execute() {
 //-----------------------------------------------------------------------------
 
 func init() {
+
+	// Define the flags
 	rootCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
+	if rootCmd.PersistentFlags().String("context", "", "Regex to match the context name.") != nil {
+		if err := rootCmd.RegisterFlagCompletionFunc("context", contextCompletionFunc); err != nil {
+			panic(err)
+		}
+	}
+}
+
+//-----------------------------------------------------------------------------
+// contextCompletionFunc
+//-----------------------------------------------------------------------------
+
+func contextCompletionFunc(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+
+	// For demonstration, let's assume:
+	contexts := []string{"context1", "context2", "context3"}
+
+	var completions []string
+	for _, context := range contexts {
+		if strings.HasPrefix(context, toComplete) {
+			completions = append(completions, context)
+		}
+	}
+	return completions, cobra.ShellCompDirectiveNoFileComp
 }
