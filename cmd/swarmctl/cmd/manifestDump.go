436e6a7b452119ec3efeef175993ae29ae0ec720
Marc Villacorta <h0tbird@github.com>
2024-01-25 17:34:39 +0100

    Introducing ValidArgs

--- cmd/swarmctl/cmd/manifestDump.go
+++ cmd/swarmctl/cmd/manifestDump.go
@@ -1,18 +1,37 @@
 package cmd
 
-import "github.com/spf13/cobra"
+import (
+
+	// Stdlib
+	"fmt"
+
+	// Community
+	"github.com/spf13/cobra"
+)
 
 //-----------------------------------------------------------------------------
-// generateCmd represents the generate command
+// dumpCmd represents the generate command
 //-----------------------------------------------------------------------------
 
 var dumpCmd = &cobra.Command{
-	Use:   "dump",
-	Short: "Dumps templates to ~/.swarmctl or stdout.",
-}
+	Use:       "dump [informer|worker]",
+	Short:     "Dumps templates to ~/.swarmctl or stdout.",
+	ValidArgs: []string{"informer", "worker"},
+	Args:      cobra.MatchAll(cobra.ExactArgs(1), cobra.OnlyValidArgs),
+	Run: func(cmd *cobra.Command, args []string) {
 
-// TODO: Use a positional argument to specify the template to dump.
-// It can be either informer or worker.
+		component := args[0]
+
+		stdout, _ := cmd.Flags().GetBool("stdout")
+		if stdout {
+			// Add logic to handle the dump to stdout here
+			fmt.Printf("Dumping %s template to stdout...\n", component)
+		} else {
+			// Add logic to handle the dump to file or other output here
+			fmt.Printf("Dumping %s template to stdout...\n", component)
+		}
+	},
+}
 
 //-----------------------------------------------------------------------------
 // init
@@ -20,4 +39,5 @@ var dumpCmd = &cobra.Command{
 
 func init() {
 	manifestCmd.AddCommand(dumpCmd)
+	dumpCmd.Flags().BoolP("stdout", "", false, "Output to stdout")
 }
